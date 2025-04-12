41a16b78a49774521a49345dfb680f05b97b03c4
MangelC25 <150280388+MangelC25@users.noreply.github.com>
2025-04-12 18:21:14 -0500

    Inicial commit: Configuración base del proyecto Laravel y entorno React
    - Creación de la estructura inicial del proyecto con Laravel 12.
    
    - Configuración del entorno de desarrollo, incluyendo Vite y la integración de React usando JSX.
    
    - Implementación de scaffolding básico para autenticación y conexión con la base de datos (MySQL).

---
+++ resources/js/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Bienvenido a Prueba Técnica</h1>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('react-root'));
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Prueba Técnica s</title>
+    @viteReactRefresh
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+</head>
+<body>
+    <div id="react-root"></div>
+</body>
+</html>
